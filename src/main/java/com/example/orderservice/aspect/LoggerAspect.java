0211f7e206375c91620a687fc6dd7db5b98890aa
HelgaLnz <helga.lanz@yandex.ru>
2024-04-02 16:12:58 +0300

    add: - LoggerAspect; - log4j2 configuration

--- src/main/java/com/example/orderservice/aspect/LoggerAspect.java
+++ src/main/java/com/example/orderservice/aspect/LoggerAspect.java
@@ -1,11 +1,9 @@
 package com.example.orderservice.aspect;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -15,18 +13,22 @@ import java.util.Arrays;
 @Log4j2
 public class LoggerAspect {
 
-  @Pointcut("execution(public * com.example.orderservice..*(..))")
-  public void execMethodsOrderAndUserService(){}
+  @Pointcut("execution(public * com.example.orderservice.service.*.*(..))")
+  public void execMethodsOrderAndUserService() {
+  }
 
   @Before("execMethodsOrderAndUserService()")
-  public void beforeExecMethodsOrderAndUserService(JoinPoint point){
-    log.info("Before methods: " + point.getSignature().getName() +
+  public void beforeExecMethodsOrderAndUserService(JoinPoint point) {
+    log.log(Level.toLevel("INFO METHOD ASPECT"), "Before methods: " + point.getSignature().getName() +
       " Args:" + Arrays.toString(point.getArgs()) +
       " Location: " + point.getSignature().getDeclaringType().getSimpleName());
   }
 
   @After("execMethodsOrderAndUserService()")
-  public void afterExecMethodsOrderAndUserService(JoinPoint point){
-    log.info("After: " + point.toString());
+  public void afterExecMethodsOrderAndUserService(JoinPoint point) {
+    log.log(Level.toLevel("INFO METHOD ASPECT"),"After methods: " + point.getSignature().getName() +
+      " Args:" + Arrays.toString(point.getArgs()) +
+      " Location: " + point.getSignature().getDeclaringType().getSimpleName());
   }
+
 }
